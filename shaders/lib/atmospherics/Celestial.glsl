1c478e441b1450ec3bc6a5ca39456f527ca260ba
HaringPro <132074996+HaringPro@users.noreply.github.com>
2025-01-27 21:13:40 +0800

    Undo "Fake sun bloom"

--- shaders/lib/atmospherics/Celestial.glsl
+++ shaders/lib/atmospherics/Celestial.glsl
@@ -1,5 +1,5 @@
 
-#define SUN_RADIUS_ENLARGING 1.0 // Enlarging factor of the sun radius (1.0 = real sun radius). [1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0 11.0 12.0 13.0 14.0 15.0 16.0 17.0 18.0 19.0 20.0 21.0 22.0 23.0 24.0 25.0 26.0 27.0 28.0 29.0 30.0 31.0 32.0 33.0 34.0 35.0 36.0 37.0 38.0 39.0 40.0]
+#define SUN_RADIUS_ENLARGING 2.0 // Enlarging factor of the sun radius (1.0 = real sun radius). [1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0 11.0 12.0 13.0 14.0 15.0 16.0 17.0 18.0 19.0 20.0 21.0 22.0 23.0 24.0 25.0 26.0 27.0 28.0 29.0 30.0 31.0 32.0 33.0 34.0 35.0 36.0 37.0 38.0 39.0 40.0]
 
 #define STARS_INTENSITY 0.1 // [0.0 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0]
 #define STARS_COVERAGE  0.1 // [0.0 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0]
@@ -24,14 +24,16 @@ vec3 RenderSun(in vec3 worldDir, in vec3 sunVector) {
         vec3 finalLuminance = sunIlluminance * factor;
 
         return finalLuminance;
+    #if 0
     } else {
         // Fake sun bloom from https://www.shadertoy.com/view/slSXRW
         float offset = cosRadius - cosTheta;
-        float gaussianBloom = 1e-3 * exp2(-offset * 1e5);
-        float invBloom = 4e-6 / (2e-2 + offset * 3e2);
+        float gaussianBloom = 4e-4 * exp2(-offset * 6e4);
+        float invBloom = 4e-4 / (1.0 + offset * 1e5);
         vec3 sunBloom = sunIlluminance * (gaussianBloom + invBloom);
 
         return sunBloom;
+    #endif
     }
 }
 
