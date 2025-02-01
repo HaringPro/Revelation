29c609cdce59c6ad51355f8209e61cc2f4a225bc
HaringPro <132074996+HaringPro@users.noreply.github.com>
2025-02-01 13:01:00 +0800

    Fix SPECULAR_MAPPING

--- shaders/settings.glsl
+++ shaders/settings.glsl
@@ -170,6 +170,9 @@ const float minCloudAbsorption	  = 0.02;
 		#undef NORMAL_MAPPING
 	#endif
 
+	#ifdef SPECULAR_MAPPING
+	#endif
+
 	#define DEFAULT_DIELECTRIC_F0 0.04 // Default dielectric F0. [0.0 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0]
 
 /* Auto Generated Normal */
