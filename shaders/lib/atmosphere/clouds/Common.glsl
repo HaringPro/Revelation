6104e8fbef4b46fd33d046b5938a3bc60b2d2f0e
HaringPro <132074996+HaringPro@users.noreply.github.com>
2025-07-07 14:52:11 +0800

    More cloud settings

--- shaders/lib/atmosphere/clouds/Common.glsl
+++ shaders/lib/atmosphere/clouds/Common.glsl
@@ -35,7 +35,11 @@
     #define CLOUD_AERIAL_PERSPECTIVE            // Enables aerial perspective for clouds
 
     #define CLOUD_WIND_SPEED 			0.005   // Wind speed of clouds. [0.0 0.0001 0.0005 0.001 0.002 0.003 0.004 0.005 0.006 0.007 0.008 0.009 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0 11.0 12.0 13.0 14.0 15.0 16.0 17.0 18.0 19.0 20.0 25.0 30.0 35.0 40.0 45.0 50.0]
+
     #define CLOUD_MS_COUNT              4       // Times of multi-scattering for clouds. [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 25 30 35 40 45 50]
+    #define CLOUD_MS_FALLOFF_S          0.5     // Scattering falloff for multi-scattering. [0.0 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0]
+    #define CLOUD_MS_FALLOFF_E          0.5     // Extinction falloff for multi-scattering. [0.0 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0]
+    #define CLOUD_MS_FALLOFF_P          0.5     // Asymmetry falloff for multi-scattering. [0.0 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0]
 
 /* Low-level clouds */
     #define CLOUD_CUMULUS 	                    // Enables cumulus clouds
@@ -92,9 +96,9 @@
 const uint  cloudMsCount 			= CLOUD_MS_COUNT;
 
 // Must be a <= b to keep energy conservation
-const float cloudMsFalloffA 	    = 0.5;
-const float cloudMsFalloffB 	    = 0.5;
-const float cloudMsFalloffC 	    = 0.5;
+const float cloudMsFalloffA 	    = CLOUD_MS_FALLOFF_S;
+const float cloudMsFalloffB 	    = CLOUD_MS_FALLOFF_E;
+const float cloudMsFalloffC 	    = CLOUD_MS_FALLOFF_P;
 
 const float cloudMapCovDist 		= 256e3; // m
 
