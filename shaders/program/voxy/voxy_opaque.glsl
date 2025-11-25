23fa50b429521a7d606e3cf25253ca65f03f372b
Small_Ku <small_ku@outlook.com>
2025-11-25 21:24:16 +0800

    feat: basic voxy water support

--- shaders/program/voxy/voxy_opaque.glsl
+++ shaders/program/voxy/voxy_opaque.glsl
@@ -2,6 +2,8 @@
 
 //======// Utility //=============================================================================//
 
+// #include "/lib/utility/Load.glsl"
+// #define loadDepthOpaqueLod(texel) 			texelFetch(vxDepthTexOpaque, texel, 0).x
 #include "/lib/utility/Math.glsl"
 #include "/lib/utility/Pack.glsl"
 
@@ -16,6 +18,15 @@ layout (location = 2) out vec4 normalOut;
 float bayer2 (vec2 a) { a = 0.5 * floor(a); return fract(1.5 * fract(a.y) + a.x); }
 #define bayer4(a) (bayer2(0.5 * (a)) * 0.25 + bayer2(a))
 
+// #include "/lib/universal/Transform.glsl"
+// vec3 ProjectDivide(in vec3 v, in mat4 m) {
+// 	return projMAD(m, v) * rcp(m[2].w * v.z + m[3].w);
+// }
+// vec3 ScreenToViewSpaceRawVoxy(in vec3 screenPos) {
+// 	vec3 NDCPos = screenPos * 2.0 - 1.0;
+// 	return ProjectDivide(NDCPos, vxProjInv);
+// }
+
 //======// Main //================================================================================//
 
 void voxy_emitFragment(VoxyFragmentParameters parameters) {
@@ -23,13 +34,20 @@ void voxy_emitFragment(VoxyFragmentParameters parameters) {
 	vec3 color = parameters.sampledColour.rgb * parameters.tinting.rgb;
 
 	albedoOut = vec4(color, 1.0);
-	// TODO: Need WorldPos /* Terrain noises */ {
+
+    // ivec2 screenTexel = ivec2(parameters.uv);
+    // vec2 screenCoord = parameters.uv * viewPixelSize;
+	// vec3 screenPos = vec3(screenCoord, loadDepthOpaqueLod(screenTexel));
+	// vec3 viewPos = ScreenToViewSpaceRawVoxy(screenPos);
+	// vec3 worldPos = transMAD(vxModelViewInv, viewPos);
+
+	// Seems not needed for voxy /* Terrain noises */ {
 	// 	const float res = 8.0;
 	// 	const float strength = 0.25;
 
 	// 	mat3 tbnMatrix = ConstructTBN(flatNormal);
 
-	// 	vec2 coord = ((parameters.tile + cameraPosition) * tbnMatrix).xy * (res / 256.0);
+	// 	vec2 coord = ((worldPos + cameraPosition) * tbnMatrix).xy * (res / 256.0);
 	// 	float noise = texture(noisetex, coord).x * 2.0;
 
 	// 	albedoOut.rgb = saturate(albedoOut.rgb * mix(1.0, noise, strength));
--- shaders/program/voxy/voxy_translucent.glsl
+++ shaders/program/voxy/voxy_translucent.glsl
@@ -1,11 +1,13 @@
 #include "/settings.glsl"
 
-// #define PASS_DH_WATER
+#define PASS_VOXY_WATER
 
 //======// Utility //=============================================================================//
 
 // #include "/lib/utility/Load.glsl"
-#define loadDepth0(texel) 			texelFetch(depthtex0, texel, 0).x
+#define loadDepthTransLod(texel) 	texelFetch(vxDepthTexTrans, texel, 0).x
+#define loadDepthOpaqueLod(texel)	texelFetch(vxDepthTexOpaque, texel, 0).x
+
 #include "/lib/utility/Math.glsl"
 #include "/lib/utility/Pack.glsl"
 
@@ -15,76 +17,55 @@ layout (location = 0) out uvec4 materialOut;
 layout (location = 1) out vec4 normalOut;
 layout (location = 2) out vec4 waterOut;
 
-//======// SSBO //================================================================================//
-
-// #include "/lib/universal/SSBO.glsl"
-
 //======// Function //============================================================================//
 
 // #include "/lib/universal/Transform.glsl"
+vec3 ProjectDivide(in vec3 v, in mat4 m) {
+	return projMAD(m, v) * rcp(m[2].w * v.z + m[3].w);
+}
+vec3 ScreenToViewSpaceRawVoxy(in vec3 screenPos) {
+	vec3 NDCPos = screenPos * 2.0 - 1.0;
+	return ProjectDivide(NDCPos, vxProjInv);
+}
+
 // #include "/lib/universal/Random.glsl"
 float bayer2 (vec2 a) { a = 0.5 * floor(a); return fract(1.5 * fract(a.y) + a.x); }
 #define bayer4(a) (bayer2(0.5 * (a)) * 0.25 + bayer2(a))
 
-// #define PHYSICS_OCEAN_SUPPORT
-
-// #ifdef PHYSICS_OCEAN
-// 	#define PHYSICS_FRAGMENT
-// 	#include "/lib/water/PhysicsOceans.glsl"
-// #else
-// 	#include "/lib/water/WaterWave.glsl"
-// #endif
-
 //======// Main //================================================================================//
 void voxy_emitFragment(VoxyFragmentParameters parameters) {
-	ivec2 texel = ivec2(parameters.uv);
-	// if (loadDepth0(texel) < 1.0) { discard; return; }
-
 	vec3 flatNormal = vec3(uint((parameters.face>>1)==2), uint((parameters.face>>1)==0), uint((parameters.face>>1)==1)) * (float(int(parameters.face)&1)*2-1);
 	vec4 vertColor = parameters.sampledColour * parameters.tinting;
 	uint materialID = uint(parameters.customId - 10000) == 3u ? 3u : 2u;
 
 	normalOut.xy = OctEncodeUnorm(flatNormal);
 
-// 	if (materialID == 3u) { // water
-// 		vec3 worldDir = normalize(worldPos - gbufferModelViewInverse[3].xyz);
+	if (materialID == 3u) { // water
+		ivec2 texel = ivec2(parameters.uv);
+    	vec2 screenCoord = parameters.uv * viewPixelSize;
+		vec3 screenPos = vec3(screenCoord, loadDepthTransLod(texel));
+		vec3 viewPos = ScreenToViewSpaceRawVoxy(screenPos);
 
-// 		#ifdef PHYSICS_OCEAN
-// 			WavePixelData wave = physics_wavePixel(physics_localPosition.xz, physics_localWaviness, physics_iterationsNormal, physics_gameTime);
+		viewPos = viewPos - vxProj[3].yzw;
 
-// 			vec3 worldNormal = wave.normal;
-// 		#else
-// 			const mat3 tbnMatrix = mat3(
-// 				vec3(1.0, 0.0, 0.0),
-// 				vec3(0.0, 0.0, 1.0),
-// 				vec3(0.0, 1.0, 0.0)
-// 			);
+		vec3 worldPos = transMAD(vxModelViewInv, viewPos);
 
-// 			vec3 minecraftPos = worldPos + cameraPosition;
-// 			#ifdef WATER_PARALLAX
-// 				vec3 worldNormal = CalculateWaterNormal(minecraftPos, worldDir * tbnMatrix);
-// 			#else
-// 				vec3 worldNormal = CalculateWaterNormal(minecraftPos);
-// 			#endif
 
-// 			worldNormal = tbnMatrix * worldNormal;
-// 		#endif
+		float depthOpaque = loadDepthOpaqueLod(texel);
+		vec3 viewPosOpaque = ScreenToViewSpaceRawVoxy(vec3(parameters.uv * viewPixelSize, depthOpaque));
+		vec3 worldPosOpaque = transMAD(vxModelViewInv, viewPosOpaque);
 
-// 		float depth1 = loadDepthOpaqueLod(texel);
-// 		vec3 viewPos1 = ScreenToViewSpace(vec3(gl_FragCoord.xy * viewPixelSize, depth1));
-// 		vec3 worldPos1 = transMAD(gbufferModelViewInverse, viewPos1);
+		vec2 encodedNormal = normalOut.xy;
+		normalOut.zw = encodedNormal;
 
-// 		vec2 encodedNormal = OctEncodeUnorm(worldNormal);
-// 		normalOut.zw = encodedNormal;
-
-// 		waterOut = vec4(distance(worldPos, worldPos1) * r255, Packup2x8(encodedNormal), 0.0, 1.0);
-// 	} else {
+		waterOut = vec4(distance(worldPos, worldPosOpaque) * r255, Packup2x8(encodedNormal), 0.0, 1.0);
+	} else {
 		normalOut.zw = normalOut.xy;
 
 		materialOut.z = Packup2x8U(vertColor.xy);
 		materialOut.w = Packup2x8U(vertColor.zw);
 		waterOut = vec4(0.0);
-// 	}
+	}
 
 	materialOut.x = PackupDithered2x8U(parameters.lightMap, bayer4(parameters.uv));
 	materialOut.y = materialID;
