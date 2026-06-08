d1105afc3e87b29b9ec0d20c1d62846905059aae
187J3X1-114514 <smjwoaini@qq.com>
2026-06-08 18:50:10 +0800

    fix voxy

--- shaders/program/voxy/Translucent.glsl
+++ shaders/program/voxy/Translucent.glsl
@@ -21,6 +21,10 @@ vec3 ScreenToViewPos(vec3 screenPos) {
 	return projectAndDivide(vxProjInv, ndcPos);
 }
 
+vec2 VoxyScreenCoord(vec2 fragCoord) {
+	return fragCoord / vec2(textureSize(vxDepthTexOpaque, 0));
+}
+
 float BlueNoise(ivec2 texel, int frame) {
 	float base = texelFetch(noisetex, texel & 255, 0).a;
 	#ifdef TAA_ENABLED
@@ -32,7 +36,7 @@ float BlueNoise(ivec2 texel, int frame) {
 
 void voxy_emitFragment(VoxyFragmentParameters parameters) {
 	ivec2 texelPos = ivec2(gl_FragCoord.xy);
-	vec2 screenCoord = gl_FragCoord.xy * scaledTexelSize;
+	vec2 screenCoord = VoxyScreenCoord(gl_FragCoord.xy);
 
 	vec4 baseColor = parameters.sampledColour * parameters.tinting;
 	vec2 lightmap = vec2(0.0, saturate((parameters.lightMap.y - 0.03125) * 1.06667));
